4f3a93e58daedbd4786791a95e21acc8617dd5ed
YasCueLav <yaslavcue@gmail.com>
2017-11-06 15:21:13 -0300

    Listo

--- Juego de memoria.playground/Contents.swift
+++ Juego de memoria.playground/Contents.swift
@@ -2,4 +2,20 @@
 
 import UIKit
 
-var str = "Hello, playground"
+
+var numero = 0...100
+
+for i in numero {
+    var x = i % 5
+    var y = i % 2
+    
+    if i >= 30 && i <= 40 {
+        print("\(i) Viva Swift!!!")
+    } else if x == 0 {
+        print("\(i) Bingo!!!")
+    } else if y == 0 {
+        print("\(i) Par!!!")
+    } else {
+        print("\(i) Impar!!!")
+    }
+}
